28a948fad933782e3f55c126d290bf4951546946
Andrew Hazelden <andrew@andrewhazelden.com>
2025-05-10 22:10:38 -0300

    card, cube, cylinder, sphere
    Added rotate attribute parsing

--- app/nusion/model/nodes/nuke_to_fusion/Card.py
+++ app/nusion/model/nodes/nuke_to_fusion/Card.py
@@ -11,39 +11,23 @@ def convert(node):
     nuke_effect_attribs = node.effect_attribs
     fusion_effect_attribs = {}
 
-    # Set default values
-    fusion_effect_attribs["\t\t\tToRed"] = "Input {Value = 5, }" # 5: Red BG
-    fusion_effect_attribs["\t\t\tToGreen"] = "Input {Value = 6, }" # 6: Green BG
-    fusion_effect_attribs["\t\t\tToBlue"] = "Input {Value = 7, }" # 7: Blue BG
-    fusion_effect_attribs["\t\t\tToAlpha"] = "Input {Value = 8, }" # 8: Alpha BG
-
     for knob in nuke_effect_attribs:
         value = nuke_effect_attribs[knob]
 
-        if knob == "channels":
-            # This is a duplicate of the conversion found in CommonAttributes.
-            # The fusion ChannelBoolean node will have the channels disabled in two places.
-            # This may cause confusion for the user.
-            # TODO: Raise minor warning about this.
-            if value == "all":
-                #TODO: Add support for fusion auxiliary channels.
-                #TODO: Flag to user if there are any extra channels in the pipe.
-                pass
-            if value == "rgb":
-                fusion_effect_attribs["\t\t\tToAlpha"] = "Input {Value = 4, }" # 4: Do Nothing
-            if value == "alpha":
-                fusion_effect_attribs["\t\t\tToRed"] = "Input {Value = 4, }" # 4: Do Nothing
-                fusion_effect_attribs["\t\t\tToGreen"] = "Input {Value = 4, }" # 4: Do Nothing
-                fusion_effect_attribs["\t\t\tToBlue"] = "Input {Value = 4, }" # 4: Do Nothing
-            if value.startswith("{"): #individual channels selected
-                if "-rgba.red" in value:
-                    fusion_effect_attribs["\t\t\tToRed"] = "Input {Value = 4, }" # 4: Do Nothing
-                if "-rgba.green" in value:
-                    fusion_effect_attribs["\t\t\tToGreen"] = "Input {Value = 4, }" # 4: Do Nothing
-                if "-rgba.blue" in value:
-                    fusion_effect_attribs["\t\t\tToBlue"] = "Input {Value = 4, }" # 4: Do Nothing
-                if "-rgba.alpha" in value or "rgba.alpha" not in value:
-                    fusion_effect_attribs["\t\t\tToAlpha"] = "Input {Value = 4, }" # 4: Do Nothing
+        if knob == "rotate":
+            # rotate {45 45 0}
+
+            if value.startswith("{"): # Multiple channels
+                split_value = value.replace("{", "").replace("}", "").split(" ")
+                # print("Rotation Values]")
+                # print(len(split_value))
+                if len(split_value) == 3:
+                    fusion_effect_attribs["\t\t\t['Transform3DOp.Rotate.X']"] = \
+                        f"Input {{Value = {split_value[0]}, }}"
+                    fusion_effect_attribs["\t\t\t['Transform3DOp.Rotate.Y']"] = \
+                        f"Input {{Value = {split_value[1]}, }}"
+                    fusion_effect_attribs["\t\t\t['Transform3DOp.Rotate.Z']"] = \
+                        f"Input {{Value = {split_value[2]}, }}"
 
     return fusion_effect_attribs
 
--- app/nusion/model/nodes/nuke_to_fusion/Cube.py
+++ app/nusion/model/nodes/nuke_to_fusion/Cube.py
@@ -11,12 +11,25 @@ def convert(node):
     nuke_effect_attribs = node.effect_attribs
     fusion_effect_attribs = {}
 
-    fusion_effect_attribs["\t\t\t['Shape']"] = f"Input {{ Value = FuID {{ 'SurfaceCubeInputs' }}, }}"
-
     for knob in nuke_effect_attribs:
         value = nuke_effect_attribs[knob]
 
-    # No attributes on this node other than channels which is converted in CommonAttributes.
+        if knob == "rotate":
+            # rotate {45 45 0}
+
+            if value.startswith("{"): # Multiple channels
+                split_value = value.replace("{", "").replace("}", "").split(" ")
+                # print("Rotation Values]")
+                # print(len(split_value))
+                if len(split_value) == 3:
+                    fusion_effect_attribs["\t\t\t['Transform3DOp.Rotate.X']"] = \
+                        f"Input {{Value = {split_value[0]}, }}"
+                    fusion_effect_attribs["\t\t\t['Transform3DOp.Rotate.Y']"] = \
+                        f"Input {{Value = {split_value[1]}, }}"
+                    fusion_effect_attribs["\t\t\t['Transform3DOp.Rotate.Z']"] = \
+                        f"Input {{Value = {split_value[2]}, }}"
+
+    fusion_effect_attribs["\t\t\t['Shape']"] = f"Input {{ Value = FuID {{ 'SurfaceCubeInputs' }}, }}"
 
     return fusion_effect_attribs
 
